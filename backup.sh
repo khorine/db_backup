93f133697bf7a30fec18f16143810ba5a7b51537
khorine <horine12@gmail.com>
2023-05-11 17:21:17 +0300

    added new script

--- backup.sh
+++ backup.sh
@@ -25,15 +25,16 @@ BACKUP_RETAIN_DAYS=3   ## Number of days to keep local backup copy
  
 #################################################################
  
-mkdir -p ${DB_BACKUP_PATH}/${TODAY}
+# mkdir -p ${DB_BACKUP_PATH}/${TODAY}
 echo "Backup started for database - ${DATABASE_NAME}"
  
  
+ cd /home
 mysqldump -h ${MYSQL_HOST} \
    -P ${MYSQL_PORT} \
    -u ${MYSQL_USER} \
    -p${MYSQL_PASSWORD} \
-   ${DATABASE_NAME} | gzip > ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}-${TODAY}.sql.gz
+   ${DATABASE_NAME} | gzip > ${DB_BACKUP_PATH}/${DATABASE_NAME}-${TODAY}.sql.gz
  
 if [ $? -eq 0 ]; then
   echo "Database backup successfully completed"
