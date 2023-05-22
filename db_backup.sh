ec6794ae027edeff9c0bbee23a6656250fcc1f03
Horine Kinoti <93375160+khorine@users.noreply.github.com>
2023-05-23 01:57:50 +0300

    Update db_backup.sh
    sample draft

--- db_backup.sh
+++ db_backup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DB_USER="root"
-DB_PASS="Savanna#123$$"
-DB_NAME="latesara"
+DB_USER="user"
+DB_PASS="pass"
+DB_NAME="dbname"
 
-BACKUP_DIR="/home/savanna/backup"
+BACKUP_DIR="/path-to-backup"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
 mysqldump --user=$DB_USER --password=$DB_PASS $DB_NAME > $BACKUP_DIR/$DB_NAME-$DATE.sql
